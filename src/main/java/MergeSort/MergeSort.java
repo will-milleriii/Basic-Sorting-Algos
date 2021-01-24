4d53af7acb3c89e03990f3c3c37e3c295f76993a
Will Miller <willmiller629@gmail.com>
2021-01-24 13:15:46 -0500

    beginning quick sort

--- src/main/java/MergeSort/MergeSort.java
+++ src/main/java/MergeSort/MergeSort.java
@@ -4,7 +4,8 @@ public class MergeSort {
 
     public static void main(String[] args){
         int[] input = new int[] {9, 7, 3, 1, 6, 3, 2, 6, 8, 9, 2, 3, 0};
-        MergeSort sort = new MergeSort();
+        Sorting sorting = new Sorting();
+        Sorting.sort(input);
 
         for (int i = 0; i < input.length; i++){
             System.out.println(input[i]);
--- src/main/java/MergeSort/Sorting.java
+++ src/main/java/MergeSort/Sorting.java
@@ -1,4 +1,53 @@
 package MergeSort;
 
 public class Sorting {
+
+    public static void sort(int[] inputArray){
+        sort(inputArray, 0, inputArray.length-1);
+    }
+
+    public static void sort(int[] inputArray, int start, int end){
+        if (end <= start){
+            return; //no need to traverse array anymore
+        }
+        int mid = (start + end) / 2;
+        sort(inputArray, start, mid); //sorts left half of array
+        sort(inputArray, mid + 1, end); //sorts right half of array
+        merge(inputArray, start, mid, end); //merges results of two sorted subArrays
+    }
+
+    public static void merge(int[] inputArray, int start, int mid, int end){
+        int[] tempArray = new int[end - start + 1];
+        int leftSlot = start; //index counter for left side of array
+        int rightSlot = mid + 1; //index counter for right side of the array
+        int k = 0;
+
+        while(leftSlot <= mid && rightSlot <= end){
+            if (inputArray[leftSlot] < inputArray[rightSlot]){
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+            } else {
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+            }
+            k++;
+        }
+        if (leftSlot <= mid){
+            while (leftSlot <= mid){
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+                k++;
+            }
+        } else if (rightSlot <= end) {
+            while (rightSlot <= end) {
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+                k++;
+            }
+        }
+        for (int i = 0; i < tempArray.length; i++){
+            inputArray[start + i] = tempArray[i];
+        }
+
+    }
 }

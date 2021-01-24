161eda163912bb531a918db062cf78ed84cb896e
Will Miller <willmiller629@gmail.com>
2021-01-23 23:15:54 -0500

    beginning merge sort

--- src/main/java/MergeSort/MergeSort.java
+++ src/main/java/MergeSort/MergeSort.java
@@ -3,6 +3,11 @@ package MergeSort;
 public class MergeSort {
 
     public static void main(String[] args){
+        int[] input = new int[] {9, 7, 3, 1, 6, 3, 2, 6, 8, 9, 2, 3, 0};
+        MergeSort sort = new MergeSort();
 
+        for (int i = 0; i < input.length; i++){
+            System.out.println(input[i]);
+        }
     }
 }
